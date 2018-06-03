afae4edb84703c3d55560aca6bf0a754f9c35f87
Tiago Pereira <correio.tiago@gmail.com>
2018-06-03 19:33:08 +0100

    teste

--- proj2/main.m
+++ proj2/main.m
@@ -5,6 +5,10 @@ addpath('./scripts/');
 vid = VideoReader('proj2.avi');
 firstFrame = 4835;%i=4635
 lastFrame = 5000;%i=5895
+a = {};
+
+s = struct('Area',{},'Centroid',{},'BoundingBox',{},'Eccentricity',{},'FilledImage',{});
+array = [s];
 
 for i=firstFrame:1:lastFrame
     imgbg = read(vid, i-15);
@@ -13,11 +17,12 @@ for i=firstFrame:1:lastFrame
     %imshow(binaryImage);
     outputBlobs = spatial_validation(binaryImage);
     a{i} = outputBlobs;
-    outputBlobs = time_validation(outputBlobs,a,i);
+    finalBlobs = time_validation(outputBlobs,a,i);
+    a{i} = finalBlobs;
     imshow(imgfr);
-    if(~isempty(outputBlobs))
-        for j=1:length(outputBlobs)
-            rectangle('Position',outputBlobs(j).BoundingBox,'EdgeColor',[1 1 0],'linewidth',2);
+    if(~isempty(finalBlobs))
+        for j=1:length(finalBlobs)
+            rectangle('Position',finalBlobs(j).BoundingBox,'EdgeColor',[1 1 0],'linewidth',2);
         end;
     end;
     drawnow;
