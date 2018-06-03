b778414da8df09eed85e1631c2f0217345085bbb
PedroJoaoRibeiro <pedro_ribeiro_93@hotmail.com>
2018-06-03 19:42:49 +0100

    Update time_validation.m

--- proj2/scripts/time_validation.m
+++ proj2/scripts/time_validation.m
@@ -1,23 +1,24 @@
-function finalBlobs = time_validation(outputBlobs,a,frame)
+function finalBlobs = time_validation(outputBlobs,array,frame)
     finalBlobs = [];
     for i=1:1:length(outputBlobs)
-        if(blobs(outputBlobs(i),a,frame))
+        if(blobs(outputBlobs(i),array,frame))
             finalBlobs = [finalBlobs; outputBlobs(i)];
         end;
     end;
 end
 
-function bool = blobs(actualBlob,a,frame)
+function bool = blobs(actualBlob,array,frame)
     R = 50;
     AreaDiff = 10;
     bool = false;
     xC = actualBlob.Centroid(1);
     yC = actualBlob.Centroid(2);
     areaC = actualBlob.Area;
-    for i=frame-10:1:frame
-        if(~isempty(a{i}))
-            for j=1:1:length(a{i})
-                b = a{i};
+    
+    for i=length(array)-10:1:length(array)-1
+        if(~isempty(array(i))
+            for j=1:1:length(array(i))
+                b = array(i);
                 xA = b(j).Centroid(1);
                 yA = b(j).Centroid(2);
                 areaA = b(j).Area;
