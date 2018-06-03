eccd6ed946d3541a35e1799841266b67698aeeb5
PedroJoaoRibeiro <pedro_ribeiro_93@hotmail.com>
2018-06-03 22:40:13 +0100

    Revert "Update time_validation.m"
    This reverts commit b778414da8df09eed85e1631c2f0217345085bbb.

--- proj2/scripts/time_validation.m
+++ proj2/scripts/time_validation.m
@@ -1,24 +1,23 @@
-function finalBlobs = time_validation(outputBlobs,array,frame)
+function finalBlobs = time_validation(outputBlobs,a,frame)
     finalBlobs = [];
     for i=1:1:length(outputBlobs)
-        if(blobs(outputBlobs(i),array,frame))
+        if(blobs(outputBlobs(i),a,frame))
             finalBlobs = [finalBlobs; outputBlobs(i)];
         end;
     end;
 end
 
-function bool = blobs(actualBlob,array,frame)
+function bool = blobs(actualBlob,a,frame)
     R = 50;
     AreaDiff = 10;
     bool = false;
     xC = actualBlob.Centroid(1);
     yC = actualBlob.Centroid(2);
     areaC = actualBlob.Area;
-    
-    for i=length(array)-10:1:length(array)-1
-        if(~isempty(array(i))
-            for j=1:1:length(array(i))
-                b = array(i);
+    for i=frame-10:1:frame
+        if(~isempty(a{i}))
+            for j=1:1:length(a{i})
+                b = a{i};
                 xA = b(j).Centroid(1);
                 yA = b(j).Centroid(2);
                 areaA = b(j).Area;
