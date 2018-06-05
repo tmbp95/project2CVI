49dc7f295ea8dd0ac98501718fb516d8edc3fd8a
Tiago Pereira <correio.tiago@gmail.com>
2018-06-05 16:42:28 +0100

    boas

--- proj2/main.m
+++ proj2/main.m
@@ -4,7 +4,7 @@ addpath('./scripts/');
 
 vid = VideoReader('proj2.avi');
 firstFrame = 4635;%i=4635
-lastFrame = 5895;%i=5895
+lastFrame = 4735;%i=5895
 a = {};
 
 for i=firstFrame:1:lastFrame
@@ -13,15 +13,46 @@ for i=firstFrame:1:lastFrame
     binaryImage = vessel_detection(imgbg,imgfr);
     %imshow(binaryImage);
     outputBlobs = spatial_validation(binaryImage);
-    a{i} = outputBlobs;
     if(firstFrame~=i)
-        outputBlobs = time_validation(outputBlobs,a,i);
-    end;
-    imshow(imgfr);
-    if(~isempty(outputBlobs))
-        for j=1:length(outputBlobs)
-            rectangle('Position',outputBlobs(j).BoundingBox,'EdgeColor',[1 1 0],'linewidth',2);
+        [finalBlobs, blobsToShow] = time_validation(outputBlobs,a,i);
+    else
+        finalBlobs = [];
+        blobsToShow = [];
+        for j=1:1:length(outputBlobs)
+            s = struct('Area',outputBlobs(j).Area,'Centroid',outputBlobs(j).Centroid,'Numb',1,'BoundingBox',outputBlobs(j).BoundingBox);
+            finalBlobs = [finalBlobs; s];
+            blobsToShow = [blobsToShow, s];
         end;
     end;
-    drawnow;
+    a{i} = finalBlobs;
+    disp(i);
+%     imshow(imgfr);
+%     if(~isempty(blobsToShow))
+%         for j=1:length(blobsToShow)
+%             rectangle('Position',blobsToShow(j).BoundingBox,'EdgeColor',[1 1 0],'linewidth',2);
+%         end;
+%     end;
+%     drawnow;
 end;
+
+
+%% READ TXT
+fid = fopen('movie.txt');
+
+tline = fgetl(fid);
+while ischar(tline)
+    arrayString = str2num(strsplit(tline));
+    Blobs = a{arrayString(1)};
+    for i=1:1:length(Blobs)
+        diffCentroidX = abs(Blobs(i).Centroid(1)-(arrayString(2)+(arrayString(4)/2)));
+        diffCentroidY = abs(Blobs(i).Centroid(2)-(arrayString(3)+(arrayString(5)/2)));
+        if(diffCentroidX>=0 || diffCentroidX<=5)
+            if(diffCentroidY>=0 || diffCentroidY<=5)
+                disp('boas pessoal');
+            end;
+        end;
+            
+    end;
+    tline = fgetl(fid);
+end
+fclose(fid);
--- proj2/scripts/time_validation.m
+++ proj2/scripts/time_validation.m
@@ -1,37 +1,43 @@
-function finalBlobs = time_validation(outputBlobs,a,frame)
+function [finalBlobs, blobsToShow] = time_validation(outputBlobs,a,frame)
     finalBlobs = [];
+    blobsToShow = [];
     for i=1:1:length(outputBlobs)
-        if(blobs(outputBlobs(i),a,frame))
-            finalBlobs = [finalBlobs; outputBlobs(i)];
+        [bool, numOcc] = blobs(outputBlobs(i),a,frame);
+        s = struct('Area',outputBlobs(i).Area,'Centroid',outputBlobs(i).Centroid,'Numb',numOcc,'BoundingBox',outputBlobs(i).BoundingBox);
+        finalBlobs = [finalBlobs; s];
+        if(bool)
+            blobsToShow = [blobsToShow, s];
+            
         end;
     end;
 end
 
-function bool = blobs(actualBlob,a,frame)
-    R = 10;
-    D = 5; %diff
-    treshD = 3;
-    AreaDiff = 10;
+function [bool,numOcc] = blobs(actualBlob,a,frame)
+    R = 50;
+    D = 4635; %diff
+    treshD = 0.5;
+    %AreaDiff = 10;
     bool = false;
     xC = actualBlob.Centroid(1);
     yC = actualBlob.Centroid(2);
-    areaC = actualBlob.Area;  
+   % areaC = actualBlob.Area;  
     numOcc = 0;
-    for i=frame-D:1:frame-1
-        if(~isempty(a{i}))
-            for j=1:1:length(a{i})
-                b = a{i};
-                xA = b(j).Centroid(1);
-                yA = b(j).Centroid(2);
-                areaA = b(j).Area;
-                distance = sqrt((xC-xA)^2+(yC-yA)^2);
-                if(distance<R)
-                    numOcc = numOcc + 1;
-                end;
+    i = frame-1;
+    if(~isempty(a{i}))
+        for j=1:1:length(a{i})
+            b = a{i};
+            xA = b(j).Centroid(1);
+            yA = b(j).Centroid(2);
+            %areaA = b(j).Area;
+            distance = sqrt((xC-xA)^2+(yC-yA)^2);
+            if(distance<R)
+                numOcc = numOcc + b(j).Numb + 1;
             end;
         end;
     end;
-    if(numOcc >= treshD)
+
+    val = numOcc/(frame-D);
+    if(val >= treshD)
         bool = true;
         return;
     end;
