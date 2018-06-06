585b0f3bfd4bedd5c76f531a7d6378ceb594b470
Tiago Pereira <correio.tiago@gmail.com>
2018-06-06 17:33:32 +0100

    asd

--- proj2/main.m
+++ proj2/main.m
@@ -27,13 +27,13 @@ for i=firstFrame:1:lastFrame
     end;
     a{i} = finalBlobs;
     b{i} = blobsToShow;
-%     imshow(imgfr);
-%     if(~isempty(blobsToShow))
-%         for j=1:length(blobsToShow)
-%             rectangle('Position',blobsToShow(j).BoundingBox,'EdgeColor',[1 1 0],'linewidth',2);
-%         end;
-%     end;
-%     drawnow;
+    imshow(imgfr);
+    if(~isempty(blobsToShow))
+        for j=1:length(blobsToShow)
+            rectangle('Position',blobsToShow(j).BoundingBox,'EdgeColor',[1 1 0],'linewidth',2);
+        end;
+    end;
+    drawnow;
 end;
 
 
@@ -41,26 +41,67 @@ end;
 fid = fopen('movie.txt');
 
 tline = fgetl(fid);
-TP = 0;
-TN = 0;
+precision = [];
+recall = [];
+nrLines = [];
+id = 0;
 while ischar(tline)
     arrayString = strsplit(tline);
+    oldid = id;
+    if(id~=str2double(arrayString{1})) 
+        TP = 0;
+    end;
     id = str2double(arrayString{1});
     Blobs = b{id};
     
-    for i=1:1:length(Blobs)
+    for i=1:1:length(Blobs) 
         if(Blobs(i).BoundingBox(1)<str2double(arrayString{2}))
             if(Blobs(i).BoundingBox(2)<str2double(arrayString{3}))
                 if(Blobs(i).BoundingBox(1)+Blobs(i).BoundingBox(3)>str2double(arrayString{2})+str2double(arrayString{4}))
                     if(Blobs(i).BoundingBox(2)+Blobs(i).BoundingBox(4)>str2double(arrayString{3})+str2double(arrayString{5}))
-                        TP = TP +1;
+                        TP = TP + 1; %True positive
                     end;
                 end;
             end;
-        else
-            TN = TN +1;
-        end;            
+        end;  
+    end;
+    
+    if(oldid~=str2double(arrayString{1})) 
+        precision = [precision; TP];
+        nrLines = [nrLines; 1];
+    else 
+        precision(id-4644) = TP; %true positives + false positives
+        nrLines(id-4644) = nrLines(id-4644) + 1; %true positives + false negatives
     end;
     tline = fgetl(fid);
 end
+totalCases = [];
+
+for i=4645:1:5895
+    if(~isempty(b{i}))
+        totalCases = [totalCases; length(b{i})];
+    else
+        totalCases = [totalCases; 0];
+    end;
+    
+end;
+array = [];
+array1 = [];
+TPsum = 0;
+totalCasesSum = 0;
+nrLinesSum = 0;
+for i=1:1:length(totalCases)
+    TPsum = TPsum + precision(i);
+    totalCasesSum = totalCasesSum + totalCases(i);
+    nrLinesSum = nrLinesSum + nrLines(i);
+    array = [array; TPsum/totalCasesSum];
+    array1 = [array1; TPsum/nrLinesSum]; 
+end;
 fclose(fid);
+
+
+%%
+figure;
+plot(array1,array);
+
+
